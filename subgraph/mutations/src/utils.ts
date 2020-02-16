43f6b0dae42f22426b8a6017e720bb9aac9834b2
David Kajpust <kajpustd@gmail.com>
2020-02-16 01:55:13 -0700

    All three metatransactions work in a single function

--- subgraph/mutations/src/utils.ts
+++ subgraph/mutations/src/utils.ts
@@ -2,7 +2,7 @@ import { keccak256 } from 'js-sha3'
 import { ethers, Contract, Signer, utils } from 'ethers'
 import base from 'base-x'
 
-import { signPermitNew } from './utils'
+import { signPermitNew } from './permit'
 
 const DAI_PERMIT_TYPEHASH =
   'ea2aa0a1be11a07ed86d755c93467f4f82362b452371d1ba94d1715123511acb'
@@ -196,107 +196,127 @@ export const daiPermit = async (
 ) => {
   const holderAddress = await holder.getAddress()
   const nonce = (await daiContract.nonces(holderAddress)).toString()
-  const sig = await signPermit(
-    holderAddress,
-    spenderAddress,
-    holder,
+  const domain = {
+    name: 'Dai Stablecoin',
+    version: '1',
+    chainId: '3',
+    verifyingContract: '0x4dcC2886A94566B2688931Ad939fBA6c20B47e87',
+  }
+
+  const message = {
+    holder: holderAddress,
+    spender: spenderAddress,
     nonce,
-    daiContract,
-    ethereum,
-  )
-  return sig
-}
-
-export const signPermit = async (
-  holderAddress: string,
-  spenderAddress: string,
-  holder: Signer,
-  nonce: string,
-  daiContract: Contract,
-  ethereum: ethers.providers.Web3Provider,
-) => {
-  const paddedNonce = leftPad(Buffer.from([nonce]).toString('hex'))
-  // We set expiry to 0 always, as that will be default for the TokenRegistry calling
-  const paddedExpiry = leftPad(Buffer.from([0]).toString('hex'))
-  // We set to 1 == true, as that will be default for the TokenRegistry calling
-  const paddedTrue = leftPad(Buffer.from([1]).toString('hex'))
-
-  const DAI_DOMAIN_SEPARATOR = await createDaiDomainSeparator(daiContract, ethereum)
-  const structEncoded =
-    DAI_PERMIT_TYPEHASH +
-    // abi.encode in solidity automatically removes the checksum, so we must use toLowerCase
-    leftPad(stripHexPrefix(holderAddress.toLowerCase())) +
-    leftPad(stripHexPrefix(spenderAddress.toLowerCase())) +
-    paddedNonce +
-    paddedExpiry +
-    paddedTrue
-
-  const hashedStruct = Buffer.from(
-    keccak256.arrayBuffer(Buffer.from(structEncoded, 'hex')),
-  )
-  const stringHashedStruct = hashedStruct.toString('hex')
-  const stringDomainSeparator = DAI_DOMAIN_SEPARATOR.toString('hex')
-  const digestData = '1901' + stringDomainSeparator + stringHashedStruct
-
-  // TODO: need to utilize "Sign Typed Data V4" here instead.
-  //       https://gitcoin.co/issue/MetaMask/Hackathons/2/3865
-  //       https://metamask.github.io/metamask-docs/API_Reference/Signing_Data/Sign_Typed_Data_v4
-  //       https://github.com/mosendo/gasless/blob/7688283021bbdb1c99b6951944345af0ba06e036/app/src/utils/relayer.js#L38-L79
-
-  /*  Expected digest value and first test deploy on ganache:
-      fc60391b0087e420dc8e15ae01ef93d0814572bbd80b3111248897d3a0d9f941
-  */
-  const digest = Buffer.from(keccak256(Buffer.from(digestData, 'hex')), 'hex')
-  // const signature = await holder.signMessage(digest)
-  // const splitSig = ethers.utils.splitSignature(signature)
-  // return {
-  //   r: splitSig.r,
-  //   s: splitSig.s,
-  //   v: splitSig.v,
-  // }
-
-  let sig = await ethereum.send('eth_signTypedData_v3', [holderAddress, digest])
-  console.log(sig)
-  return {
-    r: sig.r,
-    s: sig.s,
-    v: sig.v,
+    expiry: 0,
+    allowed: true,
   }
+
+  const stuff = await signPermitNew(ethereum, domain, message)
+
+  console.log(stuff)
+  const splitSig = ethers.utils.splitSignature(stuff.sig)
+
+  // const sig = await signPermit(
+  //   holderAddress,
+  //   spenderAddress,
+  //   holder,
+  //   nonce,
+  //   daiContract,
+  //   ethereum,
+  // )
+  return splitSig
 }
 
-const createDaiDomainSeparator = async (
-  daiContract: Contract,
-  ethereum: ethers.providers.Web3Provider,
-) => {
-  const domain = keccak256(
-    'EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)',
-  )
-  const daiName = 'Dai Stablecoin'
-  const daiVersion = '1'
-  const hashedName = keccak256(daiName)
-  const hashedVersion = keccak256(daiVersion)
+// export const signPermit = async (
+//   holderAddress: string,
+//   spenderAddress: string,
+//   holder: Signer,
+//   nonce: string,
+//   daiContract: Contract,
+//   ethereum: ethers.providers.Web3Provider,
+// ) => {
+//   const paddedNonce = leftPad(Buffer.from([nonce]).toString('hex'))
+//   // We set expiry to 0 always, as that will be default for the TokenRegistry calling
+//   const paddedExpiry = leftPad(Buffer.from([0]).toString('hex'))
+//   // We set to 1 == true, as that will be default for the TokenRegistry calling
+//   const paddedTrue = leftPad(Buffer.from([1]).toString('hex'))
 
-  // ChainID of uint256 9545 used for development, in bytes32
-  let chainID
-  const network = await ethereum.getNetwork()
-  const networkName = network.name
-  if (networkName == 'mainnet') {
-    chainID = 1
-  } else if (networkName == 'ropsten') {
-    chainID = 3
-  } else if (networkName == 'ganache') {
-    chainID = 9545
-  } else {
-    console.log('NO CHAIN ID, this will probably crash')
-  }
-  const paddedChainID = leftPad(chainID.toString(16))
-  const daiAddress = daiContract.address.toLowerCase()
-  const paddedDaiAddress = leftPad(stripHexPrefix(daiAddress))
-  const data = domain + hashedName + hashedVersion + paddedChainID + paddedDaiAddress
-  console.log('DAI DATA: ', data)
-  const hash = Buffer.from(keccak256.arrayBuffer(Buffer.from(data, 'hex')))
-  return hash
-}
+//   const DAI_DOMAIN_SEPARATOR = await createDaiDomainSeparator(daiContract, ethereum)
+//   const structEncoded =
+//     DAI_PERMIT_TYPEHASH +
+//     // abi.encode in solidity automatically removes the checksum, so we must use toLowerCase
+//     leftPad(stripHexPrefix(holderAddress.toLowerCase())) +
+//     leftPad(stripHexPrefix(spenderAddress.toLowerCase())) +
+//     paddedNonce +
+//     paddedExpiry +
+//     paddedTrue
+
+//   const hashedStruct = Buffer.from(
+//     keccak256.arrayBuffer(Buffer.from(structEncoded, 'hex')),
+//   )
+//   const stringHashedStruct = hashedStruct.toString('hex')
+//   const stringDomainSeparator = DAI_DOMAIN_SEPARATOR.toString('hex')
+//   const digestData = '1901' + stringDomainSeparator + stringHashedStruct
+
+//   // TODO: need to utilize "Sign Typed Data V4" here instead.
+//   //       https://gitcoin.co/issue/MetaMask/Hackathons/2/3865
+//   //       https://metamask.github.io/metamask-docs/API_Reference/Signing_Data/Sign_Typed_Data_v4
+//   //       https://github.com/mosendo/gasless/blob/7688283021bbdb1c99b6951944345af0ba06e036/app/src/utils/relayer.js#L38-L79
+
+//   /*  Expected digest value and first test deploy on ganache:
+//       fc60391b0087e420dc8e15ae01ef93d0814572bbd80b3111248897d3a0d9f941
+//   */
+//   const digest = Buffer.from(keccak256(Buffer.from(digestData, 'hex')), 'hex')
+//   // const signature = await holder.signMessage(digest)
+//   // const splitSig = ethers.utils.splitSignature(signature)
+//   // return {
+//   //   r: splitSig.r,
+//   //   s: splitSig.s,
+//   //   v: splitSig.v,
+//   // }
+
+//   let sig = await ethereum.send('eth_signTypedData_v3', [holderAddress, digest])
+//   console.log(sig)
+//   return {
+//     r: sig.r,
+//     s: sig.s,
+//     v: sig.v,
+//   }
+// }
+
+// const createDaiDomainSeparator = async (
+//   daiContract: Contract,
+//   ethereum: ethers.providers.Web3Provider,
+// ) => {
+//   const domain = keccak256(
+//     'EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)',
+//   )
+//   const daiName = 'Dai Stablecoin'
+//   const daiVersion = '1'
+//   const hashedName = keccak256(daiName)
+//   const hashedVersion = keccak256(daiVersion)
+
+//   // ChainID of uint256 9545 used for development, in bytes32
+//   let chainID
+//   const network = await ethereum.getNetwork()
+//   const networkName = network.name
+//   if (networkName == 'mainnet') {
+//     chainID = 1
+//   } else if (networkName == 'ropsten') {
+//     chainID = 3
+//   } else if (networkName == 'ganache') {
+//     chainID = 9545
+//   } else {
+//     console.log('NO CHAIN ID, this will probably crash')
+//   }
+//   const paddedChainID = leftPad(chainID.toString(16))
+//   const daiAddress = daiContract.address.toLowerCase()
+//   const paddedDaiAddress = leftPad(stripHexPrefix(daiAddress))
+//   const data = domain + hashedName + hashedVersion + paddedChainID + paddedDaiAddress
+//   console.log('DAI DATA: ', data)
+//   const hash = Buffer.from(keccak256.arrayBuffer(Buffer.from(data, 'hex')))
+//   return hash
+// }
 
 //////////////////////////
 ///// Basic Helpers //////
